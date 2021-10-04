2e4b2938539a8eb6d0ffc750a6c892894c4e3ebb
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2021-10-04 17:13:06 +0100

    Outline WebSocket application and telemtry emulator

---
+++ src/main/java/org/djna/asynch/estate/data/ThermostatReading.java
@@ -0,0 +1,7 @@
+package org.djna.asynch.estate.data;
+
+import java.util.Date;
+
+public class ThermostatReading {
+    // add data such temperature here
+}
---
+++ src/main/java/org/djna/asynch/estate/webapp/HomeMonitorController.java
@@ -0,0 +1,32 @@
+package org.djna.asynch.estate.webapp;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeMonitorController {
+    static private Logger LOGGER = Logger.getLogger(HomeMonitorController.class);
+    // enable simple server test
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    // display page that subscribes to one topic
+    @GetMapping("/monitor")
+    public String monitor(
+            @RequestParam(name="property", required=false, defaultValue="101") String property,
+            @RequestParam(name="locationSelected", required=false, defaultValue="hall") String location,
+            Model model) {
+        LOGGER.info("monitor " + property + "/" + location);
+        model.addAttribute("property", property);
+        model.addAttribute("location", location);
+        model.addAttribute("topic", "home/thermostats/" + property + "/" + location);
+        return "monitor";
+    }
+}
