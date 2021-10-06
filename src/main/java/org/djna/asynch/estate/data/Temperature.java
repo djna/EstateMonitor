743baf151a2d03c831d2e1fd949c57378cd6bf06
Amber Shand <amber.shand@aexp.com>
2021-10-06 12:21:21 +0100

    added git ignore file and created a class for thermostat readings, added changes to display data on html file

---
+++ src/main/java/org/djna/asynch/estate/data/Temperature.java
@@ -0,0 +1,41 @@
+package org.djna.asynch.estate.data;
+
+import java.util.ArrayList;
+
+public class Temperature {
+    ArrayList<Integer> thermostatReadings = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> thermostatReadings= new ArrayList<>();
+        thermostatReadings.add(1);
+        thermostatReadings.add(10);
+        thermostatReadings.add(10);
+        thermostatReadings.add(17);
+        thermostatReadings.add(20);
+        thermostatReadings.add(13);
+        thermostatReadings.add(12);
+        thermostatReadings.add(30);
+        thermostatReadings.add(33);
+        thermostatReadings.add(15);
+        thermostatReadings.add(-15);
+    }
+
+    public ArrayList<Integer> getThermostatReadings() {
+        return thermostatReadings;
+    }
+
+    public void setThermostatReadings(ArrayList<Integer> thermostatReadings) {
+        this.thermostatReadings = thermostatReadings;
+    }
+
+    @Override
+    public String toString() {
+        return "ThermostatArrayList{" +
+                "thermostatReadings=" + thermostatReadings +
+                '}';
+    }
+
+
+}
--- src/main/java/org/djna/asynch/estate/webapp/HomeMonitorController.java
+++ src/main/java/org/djna/asynch/estate/webapp/HomeMonitorController.java
@@ -2,10 +2,13 @@ package org.djna.asynch.estate.webapp;
 
 
 import org.apache.log4j.Logger;
+import org.djna.asynch.estate.data.ThermostatReading;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
 
 @Controller
 public class HomeMonitorController {
@@ -28,5 +31,18 @@ public class HomeMonitorController {
         model.addAttribute("location", location);
         model.addAttribute("topic", "home/thermostats/" + property + "/" + location);
         return "monitor";
+    }
+
+    @GetMapping("/myproperty")
+    public String myProperty(
+            @RequestParam(name="property", required=false, defaultValue="101") String property,
+            @RequestParam(name="locationSelected", required=false, defaultValue="hall") String location,
+            Model model) {
+        LOGGER.info("monitor " + property + "/" + location);
+        model.addAttribute("property", property);
+        model.addAttribute("location", location);
+        ThermostatReading reading = new ThermostatReading(property, new Date(), -5,location);
+        model.addAttribute("reading",reading);
+        return "myproperty";
     }
 }
